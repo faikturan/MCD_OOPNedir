fc1c2738284a0718a97d88ea75bea13d1a9d6722
Faik Turan <turanfaik@hotmail.com>
2021-11-26 19:00:54 +0300

    changed

--- MCD_OOPNedir/Program.cs
+++ MCD_OOPNedir/Program.cs
@@ -10,13 +10,20 @@ namespace MCD_OOPNedir
     {
         static void Main(string[] args)
         {
-            Arac bmw = new Arac("bmw", "jeep");
-            Arac mercedes = new Arac();
+            #region Araç Sınıfı İnceleme
+            Arac A1 = new Arac("Opel", "Corsa", 2010, 56000);
+            A1.AlisFiyat = 28000;
+            A1.SatisFiyat = 32000;
+            A1.MaxIndirimTutar = 1500;
+            //A1.Fiyat = Kapsülleme konusu
+            A1.FiyatAta(31000);
+            A1.Fiyat = 35000;
+            Console.WriteLine(A1.Fiyat);
+            A1.BilgileriGoruntule();
 
+            #endregion
 
-            
-
-
+            Console.ReadLine();
 
         }
     }
