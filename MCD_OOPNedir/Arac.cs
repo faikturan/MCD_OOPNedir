4fda0b395e5232ca158aea76147ee89d1fad5b8d
Faik Turan <turanfaik@hotmail.com>
2021-11-26 18:52:28 +0300

    Add project files.

---
+++ MCD_OOPNedir/Arac.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_OOPNedir
+{
+    class Arac
+    {
+        public string Marka;
+        public string Model;
+        public int ModelYil;
+        public decimal Km;
+        public int YakitTip;
+        public int VitesTip;
+        public decimal AlisFiyat;
+        public decimal SatisFiyat;
+        public decimal MaxIndirimTutar;
+        public decimal Fiyat;
+
+        public Arac()
+        { }
+
+
+        public Arac(string _marka, string _model)
+        {
+            Marka = _marka;
+            Model = _model;
+        }
+
+        public Arac(string _marka, string _model, int _modelyil)
+        {
+            Marka = _marka;
+            Model = _model;
+            ModelYil = _modelyil;
+        }
+
+        public Arac(string _marka, string _model, int _modelyil, decimal _km)
+        {
+            Marka = _marka;
+            Model = _model;
+            ModelYil = _modelyil;
+            Km = _km;
+        }
+
+        public void BilgileriGoruntule()
+        {
+            Console.WriteLine("Marka {0} - Model {1} - Model Yil {2}", Marka, Model, ModelYil);
+        }
+
+        public void FiyatAta(decimal _fiyat)
+        {
+            decimal fiyatHesap = SatisFiyat - MaxIndirimTutar;
+            if (_fiyat < fiyatHesap)
+            {
+                Console.WriteLine("Geçersiz fiyat girişi");
+            }
+            else
+            {
+                this.Fiyat = _fiyat;
+                Console.WriteLine("Fiyat Güncellendi");
+            }
+
+        }
+     
+    }
+}
---
+++ MCD_OOPNedir/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_OOPNedir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Arac bmw = new Arac("bmw", "jeep");
+            Arac mercedes = new Arac();
+
+
+            
+
+
+
+        }
+    }
+}
